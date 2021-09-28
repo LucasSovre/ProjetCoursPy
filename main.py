f02549cdce34db7cfa5d815c726eb9c2c893d13f
Your LucasSovre <lucas.sovre@yahoo.com>
2021-09-28 10:57:57 +0200

    creation du main

---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+mainFenetre = Tk() #on instentie la fenetre principale du programme
+mainFenetre.title('main') #renomme la fenetre principale
+
+mainFenetre.mainloop() #on lance l'attente de commande et de L'UI
