5c0cbc3a045c1a99f9b71afae1b6c5b112a40c94
Your LucasSovre <lucas.sovre@yahoo.com>
2021-09-29 14:07:54 +0200

    frame squelette de base

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from tkinter import *
 
-#slt
-#SIUUUU
+mainUI = Tk() #on instentie la fenetre principale du programme
+mainUI.title('main') #renomme la fenetre principale
+mainUI.geometry("1080x720")
 
-mainFenetre = Tk() #on instentie la fenetre principale du programme
-mainFenetre.title('main') #renomme la fenetre principale
 
-mainFenetre.mainloop() #on lance l'attente de commande et de L'UI
+menuBar = Menu(mainUI) #création du menu principal
+mainUI.config(menu=menuBar) #on defini le menu
+subMenuFile = Menu(menuBar) #on défini le sous menu 
+subMenuHelp = Menu(menuBar)
+menuBar.add_cascade(label='Fichier', menu=subMenuFile)
+subMenuFile.add_command(label='Importer')
+subMenuFile.add_command(label='Exporter')
+menuBar.add_cascade(label='Aide', menu=subMenuHelp)
+
+mainUI.columnconfigure(0, weight="1")#configuration des colonnes et lignes de la fenêtre principale
+mainUI.columnconfigure(1, weight="6")
+mainUI.columnconfigure(2, weight="3")
+mainUI.rowconfigure(0, weight="1")
+mainUI.rowconfigure(1 , weight="1")
+
+toolbar = Frame(mainUI, background="green") #configuration de la barre d'outils
+toolbar.grid(rowspan=2,row=0,column=0,sticky="nsew")
+
+visualization = Frame(mainUI, background="blue") #configuration du frame de canvas principal
+visualization.grid(rowspan=2,row=0,column=1,sticky="nsew")
+
+controlPanel = Frame(mainUI, background="yellow")
+controlPanel.grid(rowspan=2,row=0,column=2,sticky="nsew")
+
+mainUI.mainloop() #on lance l'attente de commande et de L'UI
