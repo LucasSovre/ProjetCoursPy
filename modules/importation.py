6e0c1155758213927d9695479de98c917ef86ae7
HenriqueMARTINS9 <henrique.mar@outlook.fr>
2021-09-29 15:29:10 +0200

    Commentaire Importation OK

--- modules/importation.py
+++ modules/importation.py
@@ -13,10 +13,10 @@ def select_files():
         filetypes=filetypes)
 
 
-    strFilenames = str(filenames).replace('(','').replace(')','').replace(',','').replace("'",'')
-    shutil.copy(strFilenames,'cache/')
-    tupleFile = strFilenames.split("/")
-    fileLenght = len(tupleFile)
-    file = tupleFile[fileLenght-1]
-    return(file)
+    strFilenames = str(filenames).replace('(','').replace(')','').replace(',','').replace("'",'') #Chaine de caractère du chemin du ficher
+    shutil.copy(strFilenames,'cache/') #Copier dans le dossier cache
+    tupleFile = strFilenames.split("/") #Liste du chemin du fichier séparer par les /
+    fileLenght = len(tupleFile) #Taille de la lsite
+    file = tupleFile[fileLenght-1] #On récupère le dernier élément de la liste qui est le nom du fichier
+    return(file) #On retourne le nom du fichier
     
