f2f950c75b2f403c55555a64ff10b0ae42ba1c19
KeroSong <donghenrique@gmail.com>
2021-10-06 17:37:26 +0200

    draw

--- modules/draw.py
+++ modules/draw.py
@@ -7,19 +7,19 @@ def get_x_and_y(event):
     global lasx, lasy
     lasx, lasy = event.x,event.y
 
-def draw(event):
+def draw_l(event,Color):
     global lasx, lasy
-    canvas.create_line((lasx, lasy, event.x, event.y), fill='red', width="2")
+    canvas.create_line((lasx, lasy, event.x, event.y), fill=Color, width="2")
     lasx, lasy = event.x, event.y
 
-def draw_r(event):
+def draw_r(event,Color):
     global lasx, lasy
-    canvas.create_rectangle((lasx, lasy, event.x, event.y), fill='red', width="2")
+    canvas.create_rectangle((lasx, lasy, event.x, event.y), fill=Color, width="2")
     lasx, lasy = event.x, event.y
     
-def draw_o(event):
+def draw_o(event,Color):
     global lasx, lasy
-    canvas.create_oval((lasx, lasy, event.x, event.y), fill='red', width="2")
+    canvas.create_oval((lasx, lasy, event.x, event.y), fill=Color, width="2")
     lasx, lasy = event.x, event.y
 
 canvas = Canvas(app,bg='white')
@@ -28,7 +28,7 @@ canvas.pack(fill='both',expand=1 )
 # Recupere la position de x1,y1 et x2,y2 a chaque clique gauche de la souris
 canvas.bind("<Button-1>",get_x_and_y)
 # Dessine sur le canvas entre la position de depart(clique gauche maintenu) et de l'arrivé (clique gauche relaché)
-canvas.bind("<B1-Motion>", draw)
+canvas.bind("<B1-Motion>", draw_l)
 
 
 
